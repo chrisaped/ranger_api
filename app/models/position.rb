df143068dee7ed8b8e5fd88717863dce49921f46
Christopher Pedersen <chrisaped@gmail.com>
2022-08-08 16:39:40 +0200

    created orders

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,6 @@
 class Position < ApplicationRecord
+  has_many :orders
+
   enum status: %i[open closed], _default: :open
   enum side: %i[long short]
 end
---
+++ db/migrate/20220808143735_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.integer :side
+      t.string :symbol
+      t.json :raw_order
+      t.integer :quantity
+      t.decimal :price
+      t.references :position, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
