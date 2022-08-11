8114bdedf61f4acace8f5ae1f498593fccae19cd
Christopher Pedersen <chrisaped@gmail.com>
2022-08-11 14:19:33 +0200

    added multipliers to target with test

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,3 +1,5 @@
 class Target < ApplicationRecord
   belongs_to :position
+
+  MULTIPLIERS = [1.0, 2.0, 3.0]
 end
--- test/models/target_test.rb
+++ test/models/target_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class TargetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create a new target" do
+    position = positions(:one)
+
+    assert_difference -> { Target.count } => 1 do
+      position.targets.create!(
+        quantity: 100,
+        price: 30.0,
+        multiplier: 2.0
+      )
+    end
+  end
 end
