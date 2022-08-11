742d98f192c3402bf7ea8c5208a69477f5f7348e
Christopher Pedersen <chrisaped@gmail.com>
2022-08-11 15:10:51 +0200

    removed positions controller

--- app/controllers/positions_controller.rb
+++
@@ -1,8 +0,0 @@
-class PositionsController < ApplicationController
-  def process
-    puts "here are the process params:"
-    p params
-    # handle the order response
-    # render json: handled_order_response
-  end
-end
