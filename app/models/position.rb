23cb04ec15546fe2b4f3df231100d63224d79f70
Christopher Pedersen <chrisaped@gmail.com>
2022-08-11 15:12:46 +0200

    added create_targets callback with tests

--- app/models/position.rb
+++ app/models/position.rb
@@ -4,4 +4,47 @@ class Position < ApplicationRecord
 
   enum status: %i[open closed], _default: :open
   enum side: %i[long short]
+
+  after_create :create_targets
+
+  private
+
+  def create_targets
+    Target::MULTIPLIERS.each do |multiplier|
+      self.targets.create!(
+        quantity: set_target_quantity(multiplier),
+        price: set_target_price(multiplier),
+        multiplier: multiplier
+      )
+    end
+  end
+
+  def set_target_quantity(multiplier)
+    one_third_quantity = (1/3.0 * initial_quantity).to_i
+    remaining_quantity = initial_quantity - (one_third_quantity + one_third_quantity)
+
+    case multiplier
+    when 1.0
+      one_third_quantity
+    when 2.0
+      one_third_quantity
+    when 3.0
+      remaining_quantity
+    else
+      one_third_quantity
+    end
+  end
+
+  def set_target_price(multiplier)
+    case multiplier
+    when 1.0
+      initial_price + risk_per_share
+    when 2.0
+      initial_price + (risk_per_share * 2)
+    when 3.0
+      initial_price + (risk_per_share * 3)
+    else
+      initial_price + risk_per_share
+    end
+  end
 end
--- test/models/position_test.rb
+++ test/models/position_test.rb
@@ -1,7 +1,85 @@
 require "test_helper"
 
 class PositionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create a position" do
+    assert_difference -> { Position.count } => 1 do
+      Position.create!(position_obj)
+    end
+  end
+
+  test "3 targets are created after a position is created" do
+    assert_difference -> { Position.count } => 1, -> { Target.count } => 3 do
+      Position.create!(position_obj)
+    end    
+  end
+
+  test "3 targets created have the correct multipliers" do
+    position = Position.create!(position_obj)
+
+    first_target = position.targets[0]
+    second_target = position.targets[1]
+    third_target = position.targets[2]
+
+    assert first_target.multiplier == 1.0
+    assert second_target.multiplier == 2.0
+    assert third_target.multiplier == 3.0
+  end
+
+  test "3 targets create have the correct quantities" do
+    position = Position.create!(position_obj)
+
+    first_target = position.targets[0]
+    second_target = position.targets[1]
+    third_target = position.targets[2]
+
+    assert first_target.quantity == 200
+    assert second_target.quantity == 200
+    assert third_target.quantity == 200
+  end
+
+  test "3 targets have the correct prices" do
+    position = Position.create!(position_obj)
+
+    first_target = position.targets[0]
+    second_target = position.targets[1]
+    third_target = position.targets[2]
+
+    assert first_target.price == 30.50
+    assert second_target.price == 31.00
+    assert third_target.price == 31.50
+  end
+
+  test "3 targets create have the correct quantities with an odd number" do
+    new_quantity = 667
+    risk_per_share = 0.45
+    position = Position.create!(position_obj({
+      initial_quantity: new_quantity,
+      current_quantity: new_quantity,
+      risk_per_share: risk_per_share
+    }))
+
+    assert position.initial_quantity == new_quantity
+    assert position.current_quantity == new_quantity
+    assert position.risk_per_share == risk_per_share
+
+    first_target = position.targets[0]
+    second_target = position.targets[1]
+    third_target = position.targets[2]
+
+    assert first_target.quantity == 222
+    assert second_target.quantity == 222
+    assert third_target.quantity == 223
+  end
+
+  def position_obj(attrs = {})
+    {
+      status: :open,
+      initial_quantity: 600,
+      symbol: 'GME',
+      side: :long,
+      current_quantity: 600,
+      initial_price: 30.0,
+      risk_per_share: 0.50    
+    }.deep_merge(attrs)
+  end
 end
