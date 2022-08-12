416ba5ae103c1bcec9779da32bcf94b765d6ade0
Christopher Pedersen <chrisaped@gmail.com>
2022-08-12 14:57:03 +0200

    using add_side callback for targets

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,5 +1,17 @@
 class Target < ApplicationRecord
   belongs_to :position
 
+  enum side: %i[buy sell]
+
   MULTIPLIERS = [1.0, 2.0, 3.0]
+
+  before_create :add_side
+
+  def add_side
+    if position.long?
+      self.side = :sell
+    else
+      self.side = :buy
+    end
+  end
 end
--- test/models/target_test.rb
+++ test/models/target_test.rb
@@ -12,4 +12,30 @@ class TargetTest < ActiveSupport::TestCase
       )
     end
   end
+
+  test "has proper side if long position" do
+    position = positions(:one)
+    assert position.long?
+
+    target = Target.create!(target_obj({ position: position }))
+
+    assert target.sell?
+  end
+
+  test "has proper side if short position" do
+    position = positions(:two)
+    assert position.short?
+
+    target = Target.create!(target_obj({ position: position }))
+
+    assert target.buy?
+  end
+
+  def target_obj(attrs = {})
+    {
+      quantity: 20,
+      price: 500.0,
+      multiplier: 2.0
+    }.deep_merge(attrs)
+  end
 end
