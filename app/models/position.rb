11e72c96a62605f3b2a306dac2ef315029b326e0
Christopher Pedersen <chrisaped@gmail.com>
2022-08-12 12:14:50 +0200

    refactored set_target_quantity & set_target_price

--- app/models/position.rb
+++ app/models/position.rb
@@ -11,40 +11,25 @@ class Position < ApplicationRecord
 
   def create_targets
     Target::MULTIPLIERS.each do |multiplier|
+      last_multiplier = Target::MULTIPLIERS[-1]
+      multipliers_length = Target::MULTIPLIERS.length
+
       self.targets.create!(
-        quantity: set_target_quantity(multiplier),
+        quantity: set_target_quantity(multiplier, last_multiplier, multipliers_length),
         price: set_target_price(multiplier),
         multiplier: multiplier
       )
     end
   end
 
-  def set_target_quantity(multiplier)
-    one_third_quantity = (1/3.0 * initial_quantity).to_i
-    remaining_quantity = initial_quantity - (one_third_quantity + one_third_quantity)
+  def set_target_quantity(multiplier, last_multiplier, multipliers_length)
+    partial_quantity = (1/last_multiplier * initial_quantity).to_i
+    remaining_quantity = initial_quantity - (partial_quantity * (multipliers_length - 1))
 
-    case multiplier
-    when 1.0
-      one_third_quantity
-    when 2.0
-      one_third_quantity
-    when 3.0
-      remaining_quantity
-    else
-      one_third_quantity
-    end
+    multiplier == last_multiplier ? remaining_quantity : partial_quantity
   end
 
   def set_target_price(multiplier)
-    case multiplier
-    when 1.0
-      initial_price + risk_per_share
-    when 2.0
-      initial_price + (risk_per_share * 2)
-    when 3.0
-      initial_price + (risk_per_share * 3)
-    else
-      initial_price + risk_per_share
-    end
+    initial_price + (risk_per_share * multiplier)
   end
 end
