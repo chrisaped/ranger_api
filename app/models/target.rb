20664d2794ec762287dead7d8facc1e38040f5b6
Christopher Pedersen <chrisaped@gmail.com>
2022-08-15 10:57:10 +0200

    using ternary

--- app/models/target.rb
+++ app/models/target.rb
@@ -2,16 +2,13 @@ class Target < ApplicationRecord
   belongs_to :position
 
   enum side: %i[buy sell]
+  enum category: %i[profit stop]
 
   MULTIPLIERS = [1.0, 2.0, 3.0]
 
   before_create :add_side
 
   def add_side
-    if position.long?
-      self.side = :sell
-    else
-      self.side = :buy
-    end
+    self.side = position.long? ? :sell : :buy
   end
 end
