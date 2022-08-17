b4c04a6c59f19390f4271c28dbe35dca25a82ad7
Christopher Pedersen <chrisaped@gmail.com>
2022-08-17 16:15:31 +0200

    order create_or_update_position working with tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,20 +3,49 @@ class Order < ApplicationRecord
 
   enum side: %i[buy sell]
 
-  def create_or_update_position(risk_per_share)
+  def create_or_update_position(risk_per_share, json_obj)
     position = Position.find_by(status: :open, symbol: symbol)
 
     if position.nil?
       position = create_position(risk_per_share)
+      # create first oco order
     else
       # position exists
-      update_position(position)
+      total_quantity = json_obj.dig('position_qty').to_i
+
+      position = position.update_quantity_from_order(total_quantity)
+      
+      update_position_targets(json_obj, total_quantity, position)
+
+      # create another oco order
     end
 
     self.position = position
   end
 
   private
+
+  def update_position_targets(json_obj, total_quantity, position)
+    order_type = json_obj.dig('order', 'type')
+    target = find_target(order_type, position)
+
+    if target
+      target.update_from_order(total_quantity)
+    else
+      puts "no target found"
+    end
+  end
+
+  def find_target(order_type, position)
+    Target.find_by(
+      quantity: quantity,
+      price: price,
+      position: position, 
+      filled: false, 
+      side: side, 
+      category: determine_target_category(order_type)
+    )
+  end
 
   def create_position(risk_per_share)
     Position.create!(
@@ -31,5 +60,16 @@ class Order < ApplicationRecord
 
   def determine_position_side
     side == 'buy' ? 'long' : 'short'
+  end
+
+  def determine_target_category(order_type)
+    case order_type
+    when 'limit'
+      :profit
+    when 'stop'
+      :stop
+    else
+      puts "order_type not supported: #{order_type}"
+    end    
   end
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -15,7 +15,9 @@ class OrderTest < ActiveSupport::TestCase
   test "create_or_update_position creates a new position" do
     order = Order.new(order_obj)
     risk_per_share = 0.50
-    json_obj = JSON.parse(new_order_json)
+
+    json = order_json('new_order')
+    json_obj = JSON.parse(json)
     
     assert_difference -> { Position.count } => 1 do
       order.create_or_update_position(risk_per_share, json_obj)
@@ -24,8 +26,64 @@ class OrderTest < ActiveSupport::TestCase
     assert_not order.position.nil?
   end
 
-  def new_order_json(attrs = {})
-    file = File.join(Rails.root, 'test', 'data_samples', 'new_order.json')
+  test "create_or_update_position updates an existing position" do
+    position = positions(:one)
+    assert position.initial_quantity == position.current_quantity
+
+    sell_order = order_json('sell_order')
+    json_obj = JSON.parse(sell_order)
+
+    quantity = json_obj.dig('qty').to_i
+    price = json_obj.dig('price').to_d 
+    side = json_obj.dig('order', 'side')
+
+    stop_target = Target.find_by(position: position, filled: false, category: 'stop')
+    assert_not stop_target.filled?
+    assert stop_target.quantity == position.current_quantity
+    assert stop_target.price == (position.initial_price - position.risk_per_share)
+    
+    order_attrs = {
+      side: side,
+      symbol: json_obj.dig('order', 'symbol'),
+      raw_order: sell_order,
+      quantity: quantity,
+      price: price
+    }
+    order = Order.new(order_attrs)
+    
+    assert_difference -> { Position.count } => 0 do
+      order.create_or_update_position(position.risk_per_share, json_obj)
+    end
+
+    position = order.position
+    total_quantity = json_obj.dig('position_qty').to_i
+    assert position.current_quantity == total_quantity
+    
+    first_profit_target = find_target(quantity, price, position, true, side, 'profit')
+    assert first_profit_target.filled?
+    
+    other_profit_targets = Target.where(position: position, filled: false, category: 'profit')
+    assert other_profit_targets.count == 2
+
+    stop_target = Target.find_by(position: position, filled: false, category: 'stop')
+    assert_not stop_target.filled?
+    assert stop_target.quantity == position.current_quantity
+    assert stop_target.price == (price - position.risk_per_share)
+  end
+
+  def find_target(quantity, price, position, filled, side, category)
+    Target.find_by(
+      quantity: quantity,
+      price: price,
+      position: position, 
+      filled: filled, 
+      side: side, 
+      category: category
+    )
+  end
+  
+  def order_json(name, attrs = {})
+    file = File.join(Rails.root, 'test', 'data_samples', "#{name}.json")
     json = File.read(file)
     json_obj = JSON.parse(json)
     json_obj.deep_merge(attrs).to_json
@@ -35,7 +93,7 @@ class OrderTest < ActiveSupport::TestCase
     {
       side: 'buy',
       symbol: 'TSLA',
-      raw_order: new_order_json,
+      raw_order: order_json('new_order'),
       quantity: 31,
       price: 863.8 
     }.deep_merge(attrs)
