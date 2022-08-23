4865a18a3ec50a905a552830dfcbcf1b9ca4fcc8
Christopher Pedersen <chrisaped@gmail.com>
2022-08-23 11:22:04 +0200

    removed calculate_risk_per_share, using decimals

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -12,25 +12,20 @@ class PositionsController < ApplicationController
   private
 
   def create_position(params)
+    quantity = params.dig('qty').to_i
+
     Position.create!(
-      initial_quantity: params.dig('qty').to_i,
+      initial_quantity: quantity,
       symbol: params.dig('symbol'),
       side: determine_position_side(params.dig('side')),
-      current_quantity: params.dig('qty').to_i,
+      current_quantity: quantity,
       initial_price: params.dig('limit_price').to_d,
-      risk_per_share: calculate_risk_per_share(
-        params.dig('limit_price').to_d,
-        params.dig('stop_price').to_d
-      )
+      initial_stop_price: params.dig('stop_price').to_d
     )    
   end
 
   def determine_position_side(order_side)
     order_side == 'buy' ? 'long' : 'short'
-  end
-
-  def calculate_risk_per_share(initial_price, stop_price)
-    (initial_price - stop_price).abs
   end
 end
 
